766733de67ee1e75b0e1cb5664dac54a3ae9a26f
sijiaoh <sijiaoh@outlook.com>
2024-01-01 10:30:28 +0900

    Set locale to ja.

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module Gemtrends
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
 
     # Don't generate system test files.
     config.generators.system_tests = nil
