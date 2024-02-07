cc107c573c886c00a22a0291097f9f7307621768
sijiaoh <sijiaoh@outlook.com>
2024-02-07 10:17:01 +0900

    Use pages#home to root page.

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @chart = Chart.new
-  end
-end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @chart = Chart.new
   end
 end
